d3cb5605dda69f5befeb2c6c8f9116c7d8bbd25e
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-10 12:45:45 -0600

    Using async await to networking

--- GithubUsers/ViewModel/UserViewModel.swift
+++ GithubUsers/ViewModel/UserViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class MainViewModel: ObservableObject {
+    @Published var users = [User]()
+    
+    var urlString: String {
+        return "https://api.github.com/users"
+    }
+    
+    init() {
+        loadData()
+    }
+}
+
+extension MainViewModel {
+    @MainActor
+    func fetchUsersAsync() async throws {
+        guard let url = URL(string: urlString) else {
+            print("DEBUG: Invalid URL")
+            return
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard (response as?  HTTPURLResponse)?.statusCode == 200 else { return }
+        guard let users = try? JSONDecoder().decode([User].self, from: data) else { return }
+        self.users = users
+        
+        
+    }
+    
+    func loadData() {
+        Task(priority: .medium) {
+            try await fetchUsersAsync()
+        }
+    }
+}
