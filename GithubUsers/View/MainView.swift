1be1236c15fa0268c29bff09811d61a370981cba
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-10 13:00:47 -0600

    Using the data from User

--- GithubUsers/View/MainView.swift
+++ GithubUsers/View/MainView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct MainView: View {
-    @State private var search = ""
+    @StateObject var viewModel = MainViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
+                List(viewModel.users) { user in
+                    Label(
+                        title: { Text(user.login) },
+                        icon: { /*@START_MENU_TOKEN@*/Image(systemName: "42.circle")/*@END_MENU_TOKEN@*/ }
+                    )
+                }
             }
             .navigationTitle("Github Users")
-            .padding()
         }
     }
 }
