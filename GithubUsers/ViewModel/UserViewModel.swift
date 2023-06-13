398d84f68921ccf2865a836e8c83f51bb3af539d
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-12 23:40:20 -0600

    Looking the best way to filter

--- GithubUsers/ViewModel/UserViewModel.swift
+++ GithubUsers/ViewModel/UserViewModel.swift
@@ -10,6 +10,30 @@ import Foundation
 class MainViewModel: ObservableObject {
     @Published var users = [User]()
     @Published var error: Error?
+    @Published var searchText = ""
+    
+
+    var searchResult: [String] {
+//        let results = users.map { $0.login.lowercased() }
+//        
+//        return searchText == "" ? results : results.filter{ $0.lowercased().contains(searchText) }
+        let myArray = users.map { $0.login.lowercased() }
+        if searchText.isEmpty {
+            return myArray
+        } else {
+            return myArray.filter { $0.lowercased().contains(searchText) }
+        }
+    }
+    
+    func otherSearch() -> [String] {
+        let array = users.map { $0.login.lowercased() }
+        if searchText.isEmpty {
+            return array
+        } else {
+            return array.filter { $0.lowercased().contains(searchText) }
+        }
+    }
+    
     
     var urlString: String {
         return "https://api.github.com/users"
