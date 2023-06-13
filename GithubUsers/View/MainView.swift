7f254cf7096d5efa708092a6b243e4c286102b8d
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-12 23:40:04 -0600

    Practicing

--- GithubUsers/View/MainView.swift
+++ GithubUsers/View/MainView.swift
@@ -9,20 +9,23 @@ import SwiftUI
 
 struct MainView: View {
     @StateObject var viewModel = MainViewModel()
-    @State private var search = ""
     
     var body: some View {
         NavigationView {
             VStack {
                 List(viewModel.users) { user in
                     Profile(image: user.avatar_url, title: user.login)
+                    
+                    
                 }
+                
             }
+            .searchable(text: $viewModel.searchText, prompt: "Search users")
             .navigationTitle("Github Users")
-            .searchable(text: $search)
-            .textInputAutocapitalization(.none)
+            
         }
     }
+    
 }
 
 #Preview {
