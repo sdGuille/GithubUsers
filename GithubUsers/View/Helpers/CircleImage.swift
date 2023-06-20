b1c828fe69aa3748f7fab0197c6e3c1a197d325e
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-20 17:49:29 -0600

    Circle image for detail view

---
+++ GithubUsers/View/Helpers/CircleImage.swift
@@ -0,0 +1,32 @@
+//
+//  CircleImage.swift
+//  GithubUsers
+//
+//  Created by Guillermo Ruiz Baires on 20/6/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: String
+    
+    var body: some View {
+        AsyncImage(url: URL(string: image)) { picture in
+            picture.resizable()
+        } placeholder: {
+            ProgressView()
+        }
+        .frame(width: 200, height: 200)
+        .clipShape(Circle())
+        .overlay {
+            Circle().stroke(.white, lineWidth: 4)
+        }
+        .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider{
+    static var previews: some View {
+        CircleImage(image: "https://avatars.githubusercontent.com/u/86071679?v=4")
+    }
+}
