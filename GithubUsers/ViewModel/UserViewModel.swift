6b157d1ec9cba88aef7fb64f3ca998485bb4b62a
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-20 13:42:05 -0600

    Fixed a get the right way to filter
    Replaced the users = [Users]()
    by: myUsers: [User] = []

--- GithubUsers/ViewModel/UserViewModel.swift
+++ GithubUsers/ViewModel/UserViewModel.swift
@@ -8,34 +8,15 @@
 import Foundation
 
 class MainViewModel: ObservableObject {
-    @Published var users = [User]()
     @Published var error: Error?
     @Published var searchText = ""
-
-
-
-//    var searchResult: [String] {
-////        let results = users.map { $0.login.lowercased() }
-////        
-////        return searchText == "" ? results : results.filter{ $0.lowercased().contains(searchText) }
-//        let myArray = users.map { $0.login.lowercased() }
-//        if searchText.isEmpty {
-//            return myArray
-//        } else {
-//            return myArray.filter { $0.lowercased().contains(searchText) }
-//        }
-//    }
-//    
-//    func otherSearch() -> [String] {
-//        let array = users.map { $0.login.lowercased() }
-//        if searchText.isEmpty {
-//            return array
-//        } else {
-//            return array.filter { $0.lowercased().contains(searchText) }
-//        }
-//    }
+    @Published private var myUsers: [User] = []
     
-    
+    var filteredUsers: [User] {
+        guard !searchText.isEmpty else { return myUsers }
+        return myUsers.filter { $0.login.localizedCaseInsensitiveContains(searchText) }
+    }
+
     var urlString: String {
         return "https://api.github.com/users"
     }
@@ -54,7 +35,7 @@ extension MainViewModel {
             let (data, response) = try await URLSession.shared.data(from: url)
             guard (response as?  HTTPURLResponse)?.statusCode == 200 else { throw CustomError.serverError }
             guard let users = try? JSONDecoder().decode([User].self, from: data) else { throw CustomError.invalidData }
-            self.users = users
+            self.myUsers = users
             
         } catch {
             self.error = error
@@ -67,17 +48,3 @@ extension MainViewModel {
         }
     }
 }
-
-
-extension MainViewModel {
-    var filteredUsers: [User] {
-        let searchText = self.searchText.lowercased() // Asegúrate de tener una propiedad @Published searchText en tu vista
-        
-        if searchText.isEmpty {
-            return users
-        } else {
-            return users.filter { $0.login.lowercased().contains(searchText) }
-        }
-    }
-}
-
