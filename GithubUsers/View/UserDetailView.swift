ffe0a0fb2476c876684bdbdbde707da2b5a832ca
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-21 21:55:45 -0600

    Trying to learn about modals

--- GithubUsers/View/UserDetailView.swift
+++ GithubUsers/View/UserDetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct UserDetailView: View {
     @EnvironmentObject var viewModel: MainViewModel
+    @Environment(\.presentationMode) var presentationMode
     var user: User
     
     var userIndex: Int {
@@ -16,9 +17,31 @@ struct UserDetailView: View {
     }
     
     var body: some View {
-        ScrollView {
-            CircleImage(image: user.avatar_url)
-                .padding(.top)
+        
+        ZStack {
+            Color.gray
+                .ignoresSafeArea(.all)
+            ScrollView {
+                CircleImage(image: user.avatar_url)
+                    .padding(.top, 100)
+                // User
+                Text(user.login)
+                    .font(.largeTitle)
+                    .bold()
+                Text("Type: \(user.type)")
+                    .font(.headline)
+                
+                if user.site_admin {
+                    Text("Admin")
+                } else {
+                    Text("Admin: False")
+                }
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onTapGesture {
+                presentationMode.wrappedValue.dismiss()
+            }
         }
     }
 }
