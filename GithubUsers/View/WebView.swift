2097bc4204580bde68e644c32f5017aa1f9d2d2e
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-06-22 22:43:27 -0600

    WebView with WebKit

---
+++ GithubUsers/View/WebView.swift
@@ -0,0 +1,22 @@
+//
+//  WebView.swift
+//  GithubUsers
+//
+//  Created by Guillermo Ruiz Baires on 22/6/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
