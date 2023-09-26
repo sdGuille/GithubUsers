46fa6c6e2493df0ae4f1ebc9d355875ebf113ee5
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-09-25 22:54:56 -0600

    This file is a service experiment
    I'm trying to separate the service from the ViewModel

--- GithubUsers/ViewModel/RepoViewModel.swift
+++ GithubUsers/ViewModel/RepoViewModel.swift
@@ -11,21 +11,14 @@ import Foundation
 class RepoViewModel: ObservableObject {
     @Published var error: Error?
     @Published var searchText = ""
-    @Published var repos: [Repo] = []
-
-    
-    var filteredUsers: [Repo] {
-        guard !searchText.isEmpty else { return repos }
-        return repos.filter { $0.owner.login.localizedCaseInsensitiveContains(searchText) }
-    }
-    
+    @Published var repos = [Repo]()
+    @Published var description = ""
     
     var repoUrl = ""
     
     init(){
         loadData()
     }
-
 }
 
 extension RepoViewModel {
@@ -35,13 +28,15 @@ extension RepoViewModel {
             guard let url = URL(string: repoUrl) else { throw CustomError.invalidURL }
             let (data, response) = try await URLSession.shared.data(from: url)
             guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw CustomError.serverError }
-            guard let returned = try? JSONDecoder().decode([Repo].self, from: data) else { throw CustomError.invalidData }
-            self.repos = returned
+            guard let returned = try? JSONDecoder().decode(Repo.self, from: data) else { throw CustomError.invalidData }
+            self.description = returned.description ?? ""
+            
             
         } catch {
             self.error = error
         }
     }
+    
     func loadData() {
         Task(priority: .medium) {
             try await fetchRepos()
