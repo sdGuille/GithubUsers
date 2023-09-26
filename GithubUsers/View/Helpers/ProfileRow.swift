107d5e17ce4ba0c2c46f36e9ed978563b96f2f4b
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-09-25 22:56:30 -0600

    Polishing the List components

--- GithubUsers/View/Helpers/ProfileRow.swift
+++ GithubUsers/View/Helpers/ProfileRow.swift
@@ -12,26 +12,38 @@ struct ProfileRow: View {
     let title: String
     
     var body: some View {
-        HStack {
-            AsyncImage(url: URL(string: image)) { picture in
-                picture.resizable()
-            } placeholder: {
-                ProgressView()
-            }
-                .frame(width: 75, height: 75)
-                .clipShape(Circle())
-                .overlay {
-                    Circle().stroke(.white, lineWidth: 1)
+        
+        ZStack {
+            VStack {
+                Text(title)
+                    .foregroundStyle(.white, .yellow)
+                    .font(.title3)
+                    .fontWeight(.heavy)
+                    .padding(.top)
+                    Spacer()
+                HStack {
+                    AsyncImage(url: URL(string: image)) { picture in
+                        picture.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                        .frame(width: 95, height: 95)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle().stroke(.white, lineWidth: 1)
+                        }
+                        .shadow(radius: 2)
+                        .padding()
                 }
-                .shadow(radius: 3)
-                .padding()
-            Text(title)
-                .font(.title3)
-                .fontWeight(.medium)
-                .padding()
-            Spacer()
-            
+                Spacer()
+            }
+            .frame(width: 165, height: 150)
+            .background(
+            RoundedRectangle(cornerRadius: 25.0))
+            .foregroundStyle(.regularMaterial)
+            .shadow(radius: 10)
         }
+        
     }
 }
 
@@ -40,5 +52,6 @@ struct Profile_Previews: PreviewProvider {
         ProfileRow(image: "https://avatars.githubusercontent.com/u/86071679?v=4",
                 title: "sdGuille"
         )
+        .previewLayout(.sizeThatFits)
     }
 }
